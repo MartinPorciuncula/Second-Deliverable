9ba8610ae826f18859ae99fbe90f795280e5065b
MartinPorciuncula <kgtprav@gmail.com>
2023-08-07 02:32:46 -0300

    First git of the Project

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import Weather from "./components/weather";
+function App() {
+  const [weatherinfo, setWeatherinfo] = useState(null);
+  const [countryInfo, setCountryInfo] = useState(null);
+  const API_KEY = "0c43b52a3676342d9ca88fef56a98f41";
+  const ImagesWeather = {
+    "01n": "bg-[url(/Images/night.jpg)]",
+    "01d": "bg-[url(/Images/day.png)]",
+    "02n": "bg-[url(/Images/few-clouded-night.jpg)]",
+    "02d": "bg-[url(/Images/few-clouded-day.jpg)]",
+    "04n": "bg-[url(/Images/clouded-night.jpg)]",
+    "04d": "bg-[url(/Images/clouded-day.jpg)]",
+    "09d": "bg-[url(/Images/rain-day.jpg)]",
+    "09n": "bg-[url(/Images/rain-night.jpg)]",
+    "10d": "bg-[url(/Images/rain-day.jpg)]",
+    "10n": "bg-[url(/Images/rain-night.jpg)]",
+    "11d": "bg-[url(/Images/thunder-day.jpg)]",
+    "11n": "bg-[url(/Images/thunder-night.jpg)]",
+    "13d": "bg-[url(/Images/snowed-day.jpg)]",
+    "13n": "bg-[url(/Images/snowed-night.jpg)]",
+    "50d": "bg-[url(/Images/misted-day.jpg)]",
+    "50n": "bg-[url(/Images/misted-night.jpg)]",
+  };
+
+  const success = (pos) => {
+    const lat = pos.coords.latitude;
+    const lon = pos.coords.longitude;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+    axios
+      .get(url)
+      .then(({ data }) => setWeatherinfo(data))
+      .catch((err) => console.log(err));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const keyValue = e.target.searcher.value;
+
+    const url2 = `https://api.openweathermap.org/data/2.5/weather?q=${keyValue}&appid=${API_KEY}`;
+
+    axios
+      .get(url2)
+      .then(({ data }) => setCountryInfo(data))
+      .catch((err) => console.log(err));
+  };
+
+  const changeCountry = countryInfo === null ? weatherinfo : countryInfo;
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
+  }, []);
+
+  return (
+    <main
+      className={`${
+        ImagesWeather[changeCountry?.weather[0].icon]
+      } min-h-screen font-lato flex justify-center align-center flex-col items-center px-4 bg-cover`}
+    >
+      <Weather
+        weatherinfo={weatherinfo}
+        countryInfo={countryInfo}
+        changeCountry={changeCountry}
+      />
+      <form onSubmit={handleSubmit}>
+        <input id="searcher" placeholder="Search Any City <3" className="text-black rounded-1xl my-4 text-[16px] p-1 px-2 outline-none" />
+        <button className=" bg-blue-400 text-black rounded-2xl my-4 text-[19px] p-1 px-6">
+          Search
+        </button>
+      </form>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Weather.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import countries from "./../assets/slim-2.json";
+
+const weather = ({ weatherinfo, countryInfo, changeCountry }) => {
+  const [isCelsius, setIsCelsius] = useState(true);
+
+  const countryfind = countries.find((country) => {
+    return country.alpha === changeCountry?.sys.country;
+  });
+  const KelvinToCelsius = (tempKelvin) => {
+    return (tempKelvin - 273.15).toFixed(1);
+  };
+
+  const KelvinToFarenheit = (tempKelvin) => {
+    return (((tempKelvin - 273.15) * 9) / 5 + 32).toFixed(1);
+  };
+
+  const handleChangeUnitTemp = () => {
+    setIsCelsius(!isCelsius);
+  };
+  /*console.log({ weatherinfo });*/
+
+  return (
+    <section className="text-center">
+      <h2 className="text-4xl mb-6 text-white">
+        {changeCountry?.name}, {countryfind?.name}
+      </h2>
+
+      <section className="grid gap-4 sm:grid-cols-[auto_auto]">
+        {/* sección superior */}
+        <section className="bg-[#84848450] p-1 rounded-2xl grid grid-cols-[auto_auto] items-center text-white">
+          <h4 className="col-span-2 text-2xl mt-4">
+            {changeCountry?.weather[0].description}
+          </h4>
+          <span className="text-8xl">
+            {isCelsius
+              ? KelvinToCelsius(changeCountry?.main.temp)
+              : KelvinToFarenheit(changeCountry?.main.temp)}
+            °{isCelsius ? "C" : "F"}
+          </span>
+          <div>
+            <img
+              src={`https://openweathermap.org/img/wn/${changeCountry?.weather[0].icon}@4x.png`}
+            />
+          </div>
+        </section>
+
+        {/* sección inferior */}
+        <section className="bg-[#84848450] p-2 rounded-2xl grid grid-cols-3 item py-4 sm:grid-cols-1 text-white">
+          <article className="flex gap-2 items-center">
+            <div className="w-[18px]">
+              <img src="./windIcon.svg" alt="" />
+            </div>
+            <span className="text-2xl">{changeCountry?.wind.speed}m/s</span>
+          </article>
+
+          <article className="flex gap-2 items-center">
+            <div className="w-[18px]">
+              <img src="./HumidityIcon.svg" alt="" />
+            </div>
+            <span className="text-2xl">{changeCountry?.main.humidity}%</span>
+          </article>
+
+          <article className="flex gap-2 items-center">
+            <div className="w-[18px]">
+              <img src="./PressureIcon.svg" alt="" />
+            </div>
+            <span className="text-2xl">{changeCountry?.main.pressure}hPa</span>
+          </article>
+        </section>
+      </section>
+      <button
+        onClick={handleChangeUnitTemp}
+        className="bg-blue-400 text-white rounded-2xl my-4 text-[19px] p-1 px-6"
+      >
+        Cambiar a {isCelsius ? "F°" : "C°"}
+      </button>
+    </section>
+  );
+};
+
+export default weather;
